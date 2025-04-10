a659b306f42e37ca452de135f5d6395fdb1546af
Ryan Akbar <ryan.akbar@tiket.com>
2025-04-10 17:59:08 +0700

    feat: remove file and folder

--- src/hooks/useAutocompleteQuery.ts
+++
@@ -1,9 +0,0 @@
-import { useQuery } from "@tanstack/react-query";
-import { getAutocompleteJSON } from "../services/autocomplete";
-
-export const useAutocompleteQuery = () => {
-  return useQuery({
-    queryKey: ['autocomplete-airport'],
-    queryFn: () => getAutocompleteJSON(),
-  });
-};
